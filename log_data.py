af07c94e540c1b3587773214d8e443df5e3e0c36
Paweł Spychalski <pawel@spychalski.info>
2013-03-30 15:33:43 +0100

    sensor output parsed to separate variables

--- log_data.py
+++ log_data.py
@@ -2,4 +2,13 @@ import subprocess
 
 p = subprocess.Popen('sudo ./dht11_sensor', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for line in p.stdout.readlines():
-    print line
+    
+    humidity = 0
+    temperature = 0
+
+    data = line.split("|")
+
+    print data[0]
+    print data[1]
+
+    #print line
