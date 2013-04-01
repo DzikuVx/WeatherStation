0f5889d6779047fe6de89b8663dcd8899f75f9a8
DzikuVx <pawel@spychalski.info>
2013-04-01 11:54:46 +0200

    config

--- website/db.inc.php
+++ website/db.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 $config ['handle'] = null;
-$config ['host'] = "10.0.0.152";
+$config ['host'] = "10.0.0.195";
 $config ['login'] = "pi_temperature";
 $config ['password'] = "pi_temperature";
 $config ['database'] = "pi_temperature";
