0b35bb46675f43698a92da8467cef4936de7623d
DzikuVx <pawel@spychalski.info>
2013-08-29 23:27:22 +0200

    Cache key refactoring

--- website/classes/Cache/Factory.php
+++ website/classes/Cache/Factory.php
@@ -75,5 +75,47 @@ class Factory implements \Interfaces\Singleton {
 		return self::$cacheInstance;
 	}
 
+}
 
+class CacheKey {
+	
+	/**
+	 * @var string
+	 */
+	private $module = '';
+	
+	/**
+	 * @var string
+	 */
+	private $property = '';
+	
+	/**
+	 * @param mixed $module
+	 * @param string $property
+	 */
+	public function __construct($module, $property) {
+		
+		if (is_object($module)) {
+			$this->module = get_class($module);
+		}else {
+			$this->module = (string) $module;
+		}
+		
+		$this->property = (string) $property;
+		
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getModule() {
+		return $this->module;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getProperty() {
+		return $this->property;
+	}
 }
