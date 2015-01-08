9ca942f6e1497c5044642c3e2133f81f3c8f77f0
DzikuVx <pawel@spychalski.info>
2015-01-08 21:07:58 +0100

    sensor_driver replaced with pigpio

--- get_data.py
+++ get_data.py
@@ -2,58 +2,23 @@
 # -*- coding: utf-8 -*- 
 
 '''
-Raspberry Pi Temperature and Humidity logger using DHT11 Sensor
+Raspberry Pi Temperature and Humidity logger using DHT22 Sensor
 Paweł Spychalski
-http://www.spychalski.info
+http://shtr.eu
 '''
 
-import subprocess
-import time
+import time, os
 import sqlite3
-import os
+import pigpio
+import DHT22
 
-db_host = "localhost"
-db_user = "pi_temperature"
-db_password = "pi_temperature"
-db_name = "pi_temperature"
-
-def getReadout(type):
-
-	if type == "internal":
-		p = subprocess.Popen('sudo /home/pi/WeatherStation/sensor_driver 11 4', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	elif type == "external":
-		p = subprocess.Popen('sudo /home/pi/WeatherStation/sensor_driver 2302 17', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	else:
-		return None
-
-	for line in p.stdout.readlines():
-
-		if len(line) < 2 or len(line) > 10:
-			print "Error from driver: " + line
-			return None
-		else:
-			return line.split("|")
-
-
-
-def saveSQLite(data, type):
+def saveSQLite(data):
 	
 	conn = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + '/data.db')
 
-	if type == "internal":
-
-		c = conn.cursor()
-		c.execute('CREATE TABLE IF NOT EXISTS readouts(`Date` text, Temperature real, Humidity real)')
-
-		c.execute("INSERT INTO readouts(`Date`, Humidity, Temperature) VALUES(datetime('now','localtime'), "+data[0]+","+data[1]+")")
-
-	elif type == "external":
-		c = conn.cursor()
-		c.execute('CREATE TABLE IF NOT EXISTS readouts_external(`Date` text, Temperature real, Humidity real)')
-
-		c.execute("INSERT INTO readouts_external(`Date`, Humidity, Temperature) VALUES(datetime('now','localtime'), "+data[0]+","+data[1]+")")
-	else:
-		return
+	c = conn.cursor()
+	c.execute('CREATE TABLE IF NOT EXISTS readouts_external(`Date` text, Temperature real, Humidity real)')
+	c.execute("INSERT INTO readouts_external(`Date`, Humidity, Temperature) VALUES(datetime('now','localtime'), " + str(data[0]) + "," + str(data[1]) + ")")
 
 	conn.commit()
 	conn.close()
@@ -66,23 +31,31 @@ def main():
 
 	counter = 0
 
+	pi = pigpio.pi()
+	sensor = DHT22.sensor(pi, 17)
+
 	while (readout == None and counter < 5):
 
 		counter += 1 
 
-		readout = getReadout("external")
+		#Get data from sensor
+		sensor.trigger()
+		time.sleep(0.2)
 
-		if readout != None:
+		humidity = sensor.humidity()
+		temperature = sensor.temperature()
 
-			saveSQLite(readout, "external")
+		if humidity != None and temperature != None and humidity >= 0 and humidity <= 100:
 
-			humidity = readout[0]
-			temperature = readout[1]
-		
-			print "Humidity: " + humidity
-			print "Temperature: " + temperature
+			readout = [humidity, temperature]
+
+			saveSQLite(readout)
+
+			print "Humidity: " + str(humidity)
+			print "Temperature: " + str(temperature)
+			counter = 10
 		else:
-			time.sleep(1)
+			time.sleep(5)
 
 if __name__ == "__main__":
     main()
--- run.sh
+++
@@ -1,7 +0,0 @@
-#set SHELL='/bin/bash'
-#set PWD='/home/pi/raspberry_temperature_log'
-
-set > /home/pi/run.log
-
-python /home/pi/raspberry_temperature_log/log_data.py
-
