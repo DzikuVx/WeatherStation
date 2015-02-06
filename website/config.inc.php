cc24109e080cfd89aa0e05fd1bf19e5816ef4080
Paweł Spychalski <pawel@spychalski.info>
2015-02-06 08:56:20 +0100

    Update config.inc.php

--- website/config.inc.php
+++ website/config.inc.php
@@ -2,7 +2,14 @@
 
 $config = array();
 
+/*
+  instead of fake variable cache following other cache mechanisms can be used:
+  - Memcached
+  - Apc
+*/
 $config['cacheMethod'] = 'Variable';
+$config['memcachedIP'] = "127.0.0.1";
+$config['memcachedPort'] = 11211
 
 $config['useLocalDataMockup'] = false;
 
