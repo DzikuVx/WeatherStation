5c84f5fef4371578b4b107b8e2b1e31782a69539
Pawel <pspychalski@gmail.com>
2015-04-30 14:11:33 +0200

    changes

--- website/common.php
+++ website/common.php
@@ -3,10 +3,10 @@
 require_once 'classes/General/Autoloader.php';
 \General\Autoloader::register();
 
-\General\Debug::$displayErrors 			= true;
-\General\Debug::$displayTrace 			= true;
-\General\Debug::$sendTrace 				= false;
-\General\Debug::$errorHoldsExecution 	= true;
-\General\Debug::$writeTrace 			= true;
-\General\Debug::$writeFile 				= 'logs/error.log';
+\General\Debug::$displayErrors = true;
+\General\Debug::$displayTrace = true;
+\General\Debug::$sendTrace  = false;
+\General\Debug::$errorHoldsExecution = true;
+\General\Debug::$writeTrace  = true;
+\General\Debug::$writeFile = 'logs/error.log';
 \General\Debug::create();
