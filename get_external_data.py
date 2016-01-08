a68b78babc6ea52760719a5330604182a9fcc37c
Pawel Spychalski <pawel.spychalski@savingglobal.com>
2016-01-08 13:16:41 +0000

    attiny light meter storage added

--- get_external_data.py
+++ get_external_data.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 '''
 Following script gets weather data from OpenWeathcerMap.org using 
 JSON API 
@@ -8,11 +11,22 @@ Data acquisited:
 - Wind Direction
 
 '''
-import urllib2, json, sqlite3, os
+import urllib2
+import json
+import sqlite3
+import os
 import Adafruit_BMP.BMP085 as BMP085
 import sensor
+import smbus
 
 from openweatherconfig import config
+from sensor_config import config as sensor_config
+
+# define I2C bus number
+I2C_BUS_NUMBER = 1
+
+# define device address
+LIGHT_SENSOR_ADDR = 0x13
 
 def fetchJSON(url):
     req = urllib2.Request(url)
@@ -53,17 +67,28 @@ def saveSQLite(data):
 
 
 def main():
-    sWeather = fetchJSON(
-        "http://api.openweathermap.org/data/2.5/weather?q=" + config['location'] + "&units=metric&APPID=" + config[
-            'api'])
-    jWeather = json.loads(sWeather)
+    
+    try:
+        sWeather = fetchJSON("http://api.openweathermap.org/data/2.5/weather?q=" + config['location'] + "&units=metric&APPID=" + config['api'])
+        jWeather = json.loads(sWeather)
 
-    data = processData(jWeather)
+        data = processData(jWeather)
 
-    saveSQLite(data)
-
-    print data
-
-
+        saveSQLite(data)
+    except urllib2.HTTPError:
+        print "Failed to connect to OpenWeathcerMap"
+    
+    if (sensor_config['attiny_light_meter'] == True):
+        try:
+            bus = smbus.SMBus(I2C_BUS_NUMBER)
+            light_level = bus.read_byte(LIGHT_SENSOR_ADDR) * 256 + bus.read_byte(LIGHT_SENSOR_ADDR);
+        
+            sensor_handler = sensor.sensor()
+            sensor_handler.save_value(7, light_level)
+        except IOError as e:
+            print "Failed to fetch light level"
+    
+    print "All data fetched"
+    
 if __name__ == "__main__":
     main()
---
+++ sensor_config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+config = {
+    'dht22': True,
+    'bmp180': True,
+    'attiny_light_meter': True,
+    'openweathermap': True
+}
