f3f7d7fc8bcb9ecb5006f6b668f721384a814de6
Frank <a4400@arcor.de>
2016-04-05 20:13:49 +0200

    Update sensor_config.py

--- sensor_config.py
+++ sensor_config.py
@@ -5,6 +5,6 @@ config = {
     'dht22': True,
     'bmp180': True,
     'attiny_light_meter': False,
-    'BH1750FVI_light_meter': True,
+    'BH1750FVI_light_meter': False,
     'openweathermap': True
 }
